e0ea56af8d0ea2548d03e1c2f202658138645c89
Moutaz <xmoutaz@hotmail.com>
2024-01-21 14:05:26 +0100

    Add error handling + tests.

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,16 @@
+// Package cmd root.go
 package cmd
 
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"os"
 )
 
-var rootCmd = &cobra.Command{
+var Root = &cobra.Command{
 	Use:   "url-shortner",
 	Short: "Link",
-	Long: `Take any lengthy URL and make it into something shorter
-
-A secondary purpose of this application is me(Moutaz Chaara) to sharpen my skills in GOLANG.
+	Long: `Take any lengthy URL and make it into something shorter.
+A secondary purpose of this application is me (Moutaz Chaara) to sharpen my skills in GOLANG.
 If prospective employers come looking, here's some
 code!`,
 
@@ -21,12 +20,14 @@ code!`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main() func. It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
+// This is called by main() func. It only needs to happen once to the root.
+func Execute(c *cobra.Command) int {
+	err := c.Execute()
 	if err != nil {
-		os.Exit(1)
+		fmt.Println("Error:", err)
+		return ExitGeneralError
 	}
+	return ExitSuccess
 }
 
 // init to declare configuration
