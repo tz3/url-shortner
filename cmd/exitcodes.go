dad12cd084800e5cba4fc2b5208ab82b16017773
Moutaz Chaara <31438677+tz3@users.noreply.github.com>
2024-01-27 22:19:47 +0100

    Connect the storage to the serve command. (#12)
    Connect the storage to the serve command. (#12)
    
    ## In this commit were the following changes:
    
    ### Additions & modifications:-
    Add parsing logic for serve command to construct the different storage
    models, in a reasonable testable way.
    
    
    
    ### Future work:-
    Will wrap the server entirely and move the server to its own package.

--- cmd/exitcodes.go
+++ cmd/exitcodes.go
@@ -1,7 +1,8 @@
 package cmd
 
 const (
-	ExitSuccess      = 0
-	ExitGeneralError = 1
-	ExitUsageError   = 64 // Used incorrectly
+	ExitSuccess               = 0
+	ExitGeneralError          = 1
+	ExitInternalSoftwareError = 505
+	ExitUsageError            = 64 // Used incorrectly
 )
